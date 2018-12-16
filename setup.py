94a78192d074fde1d8169f68d3c9d6f208626f87
Pellegrino Prevete <pellegrinoprevete@gmail.com>
2018-12-16 11:41:57 +0100

    apparently forgot some files

---
+++ setup.py
@@ -0,0 +1,48 @@
+from os import walk
+from os.path import join
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+def explore(path):
+    result = []
+    for (path, dirname, files) in walk(path):
+        for f in files: 
+            result.append(join(path, f)[5:])
+    return result
+
+daty_files = explore('daty/po') + explore('daty/resources')
+
+setup(
+    name = "daty",
+    version = "0.0.1",
+    author = "Pellegrino Prevete",
+    author_email = "pellegrinoprevete@gmail.com",
+    description = "Advanced Wikidata editor",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://gitlab.com/tallero/daty",
+    packages = find_packages(),
+    package_data = {
+        '': ['*.sh'],
+        'daty':daty_files
+    },
+    entry_points = {
+        'console_scripts': ['daty = daty:main']
+    },
+    install_requires = [
+    'appdirs',
+    'bleach',
+    'beautifulsoup4',
+    'pygobject',
+    'pywikibot',
+    'setproctitle',
+    'requests',
+    ],
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+        "Operating System :: Unix",
+    ],
+)
---
+++ win32/README.md
@@ -0,0 +1,11 @@
+# Windows installer
+
+To create the installer you need to follow the next steps:
+ - install the latest version of msys2: http://msys2.github.io/
+ - launch msys2_shell.bat and update it with pacman -Syu, you will need to
+   relaunch msys2_shell.bat after updating it
+ - install git: pacman -S git
+ - clone daty: git clone https://gitlab.com/tallero/daty
+ - cd daty/win32
+ - edit make-daty-installer and set the right version of gedit
+ - ./make-daty-installer.bat
