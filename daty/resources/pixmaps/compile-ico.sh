19d940f090660195e31663aff5bac1067152b05b
Pellegrino Prevete <pellegrinoprevete@gmail.com>
2018-12-19 03:17:32 +0100

    removed unnecessary files

--- daty/resources/pixmaps/compile-ico.sh
+++ daty/resources/pixmaps/compile-ico.sh
@@ -5,5 +5,6 @@ declare -a arr=(16 32 48 64 128 256)
 for i in "${arr[@]}"; do
     convert daty-logo.svgz -scale "$i" tmp/"$i".png
 done
-convert tmp/16.png tmp/32.png tmp/48.png tmp/64.png tmp/128.png tmp/256.png daty.ico
+#convert tmp/16.png tmp/32.png tmp/48.png tmp/64.png tmp/128.png tmp/256.png daty.ico
+convert tmp/256.png daty.ico
 rm -r tmp
