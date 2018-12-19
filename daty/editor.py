98ba8b15b531fbf879481dff49192f718c27ce25
Pellegrino Prevete <pellegrinoprevete@gmail.com>
2018-12-19 20:51:38 +0100

    nested leaflets collapse only on one

--- daty/editor.py
+++ daty/editor.py
@@ -3,13 +3,23 @@
 
 from gi import require_version
 require_version('Gtk', '3.0')
-from gi.repository.Gtk import ApplicationWindow, Template
+from gi.repository.Gtk import ApplicationWindow, IconTheme, Label, Template
+
+from .page import Page
 
 @Template.from_resource("/org/prevete/Daty/editor.ui")
 class Editor(ApplicationWindow):
     __gtype_name__ = "Editor"
 
+    content_box = Template.Child("content_box")
+
     def __init__(self, *args, **kwargs):
         ApplicationWindow.__init__(self, *args, **kwargs)
+        icon = lambda x: IconTheme.get_default().load_icon(("accessories-dictionary"), x, 0)
+        icons = [icon(size) for size in [32, 48, 64, 96]];
+        self.set_icon_list(icons);
+
+        #self.content_box.add(Page())
+
         self.show_all()
     
