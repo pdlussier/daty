bdcdb9ab79354c3a717cd9214c8b4a6caa99d883
Pellegrino Prevete <pellegrinoprevete@gmail.com>
2018-12-22 04:20:10 +0100

    responsive part finished

--- daty/editor.py
+++ daty/editor.py
@@ -12,10 +12,16 @@ class Editor(ApplicationWindow):
     __gtype_name__ = "Editor"
 
     app_menu = Template.Child("app_menu_popover")
+    item_stack = Template.Child("item_stack")
+    common = Template.Child("common-viewport")
+    label_test = Template.Child("label-test")
     content_box = Template.Child("content_box")
+    content_stack = Template.Child("content_stack")
 
     def __init__(self, *args, **kwargs):
         ApplicationWindow.__init__(self, *args, **kwargs)
+
+        # Set window icon
         icon = lambda x: IconTheme.get_default().load_icon(("daty"), x, 0)
         icons = [icon(size) for size in [32, 48, 64, 96]];
         self.set_icon_list(icons);
@@ -30,3 +36,16 @@ class Editor(ApplicationWindow):
             self.app_menu.hide()
         else:
             self.app_menu.set_visible(True)
+
+    @Template.Callback()
+    def check_resize_cb(self, widget):
+        if self.content_box.props.folded:
+            self.item_stack.set_visible_child_name("column_switcher")
+            if self.label_test in self.common:
+                self.common.remove(self.label_test)
+                self.content_stack.add_titled(self.label_test, "common", "Common")
+        else:
+            self.item_stack.set_visible_child_name("item_button")
+            if self.label_test in self.content_stack.get_children():
+                self.content_stack.remove(self.label_test)
+                self.common.add(self.label_test)
